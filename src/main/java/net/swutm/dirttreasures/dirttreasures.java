f5deb74fa720d137ab2fb5db6513f562bd161457
SWUTM <balcknutfdsfsddfsdsf@gmail.com>
2023-05-06 01:40:09 +0300

    dirttreasures-1.2-1.19-1.19.2

---
+++ src/main/java/net/swutm/dirttreasures/dirttreasures.java
@@ -0,0 +1,15 @@
+package net.swutm.dirttreasures;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class dirttreasures implements ModInitializer {
+	public static final String MOD_ID = "dirttreasures";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
